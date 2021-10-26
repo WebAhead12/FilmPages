4a2c9bfe061af2278a9225d028c11558dde57eca
julio1703 <89707649+julio1703@users.noreply.github.com>
2021-10-26 17:28:08 +0300

    adds the main fetch request for the project

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const imgURl="https://image.tmdb.org/t/p/w500/";
-
-
 const genreObj = {
   28: "Action",
   12: "Adventure",
@@ -20,11 +17,33 @@ const genreObj = {
   10770: "TV Movie",
   53: "Thriller",
   10752: "War",
-  37: "Western"
-}
+  37: "Western",
+};
 
+const imgUrl = "https://image.tmdb.org/t/p/w500/";
+const moviename1 = document.querySelector("#moviename");
+const search1 = document.querySelector("#search");
+const movieblock1 = document.querySelector(".movieblock");
+search1.addEventListener("click", (event) => {
+  movieblock1.innerHTML = "";
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=d5dc762873106644192a916a78a39251&language=en-US&query=${moviename1.value}&api_key=d5dc762873106644192page=1&include_adult=false`
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    .then((json) => {
+      const arrmovie = json.results;
+      arrmovie.forEach((movie, index) => {
+        const image = document.createElement("img");
+        image.src = `${imgUrl}${movie.poster_path}`;
+        image.alt = movie.title;
+        image.classList.add("exampleimg");
 
+        image.addEventListener("click", () => {});
 
-
-
-
+        movieblock1.appendChild(image);
+      });
+    });
+});

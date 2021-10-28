cd6dada38b0d279eb5e288a8fdeab82556370333
julio1703 <julio.elias22@gmail.com>
2021-10-28 08:53:31 +0300

    js update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const imgURl = "https://image.tmdb.org/t/p/w500/";
 
-const genreObj = {
+const genremovie = {
   28: "Action",
   12: "Adventure",
   16: "Animation",
@@ -21,49 +21,86 @@ const genreObj = {
   10752: "War",
   37: "Western",
 };
-var check = 0;
+const genretv = {
+  10759: "Action & Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  10762: "Kids",
+  9648: "Mystery",
+  10763: "News",
+  10764: "Reality",
+  10765: "Sci-Fi & Fantasy",
+  10766: "Soap",
+  10767: "Talk",
+  10768: "War & Politics",
+  37: "Western",
+};
+let check = 0, //check = 0 (default), check = 1 (top-low rating), check = 2 (upcoming)
+  index = 0, //for pagination (start)
+  total = 1, //for pagination (end)
+  page = 1;
+let isPopupOpen = false;
+let issearch = 2; // issearch = 1 (moviebyname or tvbyname) , issearch = 2(popularmovie or populartv) , issearch = 0(u cant press on any button except for search)
+const pr = document.querySelector(".paginate.left");
+const pl = document.querySelector(".paginate.right");
+const counter1 = document.querySelector(".counter");
+const upcoming1 = document.querySelector("#upcoming");
 const toplowrating = document.querySelector("#Top-Low-Rating");
-let issearch = 2; // issearch = 1 (moviebyname or tvbyname) , issearch = 2(popularmovie or populartv)
 const mvtvtoggle = document.querySelector("#togBtn"); //toggles between tv shows and movies (movie == flase , tv shows == true)
 const imgUrl = "https://image.tmdb.org/t/p/w500/";
 const searchname = document.querySelector("#moviename");
 const search1 = document.querySelector("#search");
 const movieblock1 = document.querySelector(".movieblock");
-const popup1 = document.querySelector(".popup")
-const exit = document.querySelector(".close")
-let isPopupOpen = false;
+const popup1 = document.querySelector(".popup");
+const exit = document.querySelector(".close");
 const checkList = document.getElementById("list1");
 
+getPopularMovies(); //shows popularMovies when u load the website
 
+// AddEventListeners:
+exit.addEventListener("click", closeCard); //closes popup card
 
-
-getPopularMovies();
-
-exit.addEventListener("click", closeCard)
 searchname.addEventListener("click", () => (issearch = 0));
-mvtvtoggle.addEventListener("change", (event) => {
-  if ((mvtvtoggle.checked == true && searchname.value == "" && issearch == 1) || (mvtvtoggle.checked == true && issearch == 2)) {
-    movieblock1.innerHTML = "";
+
+mvtvtoggle.addEventListener("change", () => {
+  movieblock1.innerHTML = "";
+  if (mvtvtoggle.checked == true && check == 1) {
+    ratingTopLowTv();
+  } else if (mvtvtoggle.checked == false && check == 1) {
+    ratingTopLowMovies();
+  }
+  if (mvtvtoggle.checked == true && check == 2) {
+    airingTv();
+  } else if (mvtvtoggle.checked == false && check == 2) {
+    upcomingMovie();
+  }
+  if (mvtvtoggle.checked == true && issearch == 2) {
     getPopularTvs();
   } else if (mvtvtoggle.checked == true && searchname.value != "" && issearch == 1) {
-    movieblock1.innerHTML = "";
     getTvByName(searchname.value);
-  } else if ((mvtvtoggle.checked == false && searchname.value == "" && issearch == 1) || (mvtvtoggle.checked == false && issearch == 2)) {
-    movieblock1.innerHTML = "";
+  } else if (mvtvtoggle.checked == false && issearch == 2) {
     getPopularMovies();
   } else if (mvtvtoggle.checked == false && searchname.value != "" && issearch == 1) {
-    movieblock1.innerHTML = "";
     getMovieByName(searchname.value);
   }
 });
-search1.addEventListener("click", (event) => {
+
+search1.addEventListener("click", () => {
   movieblock1.innerHTML = "";
+  toplowrating.style["background-color"] = "#f1f1f1";
+  check = 0;
+  // index = -1;
+  // page = 1;
+  // pr.setAttribute("data-state", index === 0 ? "disabled" : "");
+  counter1.innerHTML = index + 1 + " / " + total;
   if (mvtvtoggle.checked == true && searchname.value == "") {
-    movieblock1.innerHTML = "";
     getPopularTvs();
     issearch = 2;
   } else if (mvtvtoggle.checked == false && searchname.value == "") {
-    movieblock1.innerHTML = "";
     getPopularMovies();
     issearch = 2;
   }
@@ -75,25 +112,137 @@ search1.addEventListener("click", (event) => {
     issearch = 1;
   }
 });
-toplowrating.addEventListener("click", (event) => {
+
+toplowrating.addEventListener("click", () => {
   movieblock1.innerHTML = "";
-  if (mvtvtoggle.checked == true) {
-    console.log("1");
-    ratingTopLowTv();
-  } else if (mvtvtoggle.checked == false) {
-    console.log("2");
-    ratingTopLowMovies();
+  if (check == 0) {
+    check = 1;
+    toplowrating.style["background-color"] = "#ddd";
+    if (mvtvtoggle.checked == true) {
+      ratingTopLowTv();
+    } else if (mvtvtoggle.checked == false) {
+      ratingTopLowMovies();
+    }
+    return;
+  } else if (check == 1) {
+    check = 0;
+    toplowrating.style["background-color"] = "#f1f1f1";
+    if (mvtvtoggle.checked == true) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == false) {
+      getPopularMovies();
+    }
+    return;
+  }
+});
+
+upcoming1.addEventListener("click", () => {
+  movieblock1.innerHTML = "";
+  if (check == 0) {
+    check = 2;
+    upcoming1.style["background-color"] = "#ddd";
+    if (mvtvtoggle.checked == true) {
+      airingTv();
+    } else if (mvtvtoggle.checked == false) {
+      upcomingMovie();
+    }
+    return;
+  } else if (check == 2) {
+    check = 0;
+    upcoming1.style["background-color"] = "#f1f1f1";
+    if (mvtvtoggle.checked == true) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == false) {
+      getPopularMovies();
+    }
+    return;
+  }
+});
+
+pr.addEventListener("click", () => {
+  movieblock1.innerHTML = "";
+  if (page <= 1) {
+    page = 1;
+    if (mvtvtoggle.checked == true && check == 1) {
+      ratingTopLowTv();
+    } else if (mvtvtoggle.checked == false && check == 1) {
+      ratingTopLowMovies();
+    }
+    if (mvtvtoggle.checked == true && issearch == 2 && check == 0) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == true && searchname.value != "") {
+      getTvByName(searchname.value);
+    } else if (mvtvtoggle.checked == false && issearch == 2 && check == 0) {
+      getPopularMovies();
+    } else if (mvtvtoggle.checked == false && searchname.value != "") {
+      getMovieByName(searchname.value);
+    }
+  } else if (page > 1) {
+    page--;
+    if (mvtvtoggle.checked == true && check == 1) {
+      ratingTopLowTv();
+    } else if (mvtvtoggle.checked == false && check == 1) {
+      ratingTopLowMovies();
+    }
+    if (mvtvtoggle.checked == true && issearch == 2 && check == 0) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == true && searchname.value != "") {
+      getTvByName(searchname.value);
+    } else if (mvtvtoggle.checked == false && issearch == 2 && check == 0) {
+      getPopularMovies();
+    } else if (mvtvtoggle.checked == false && searchname.value != "") {
+      getMovieByName(searchname.value);
+    }
+  }
+});
+
+pl.addEventListener("click", () => {
+  movieblock1.innerHTML = "";
+  if (page >= total) {
+    page = total;
+    if (mvtvtoggle.checked == true && check == 1) {
+      ratingTopLowTv();
+    } else if (mvtvtoggle.checked == false && check == 1) {
+      ratingTopLowMovies();
+    }
+    if (mvtvtoggle.checked == true && issearch == 2 && check == 0) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == true && searchname.value != "") {
+      getTvByName(searchname.value);
+    } else if (mvtvtoggle.checked == false && issearch == 2 && check == 0) {
+      getPopularMovies();
+    } else if (mvtvtoggle.checked == false && searchname.value != "") {
+      getMovieByName(searchname.value);
+    }
+  } else if (page < total) {
+    page++;
+    if (mvtvtoggle.checked == true && check == 1) {
+      ratingTopLowTv();
+    } else if (mvtvtoggle.checked == false && check == 1) {
+      ratingTopLowMovies();
+    }
+    if (mvtvtoggle.checked == true && issearch == 2 && check == 0) {
+      getPopularTvs();
+    } else if (mvtvtoggle.checked == true && searchname.value != "") {
+      getTvByName(searchname.value);
+    } else if (mvtvtoggle.checked == false && issearch == 2 && check == 0) {
+      getPopularMovies();
+    } else if (mvtvtoggle.checked == false && searchname.value != "") {
+      getMovieByName(searchname.value);
+    }
   }
 });
 // Functions
 //gets top rated movies
 function ratingTopLowMovies() {
-  fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=1`)
+  fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((movie, index) => {
         if (movie.poster_path != null) {
@@ -112,12 +261,14 @@ function ratingTopLowMovies() {
 }
 //gets top rated tv shows
 function ratingTopLowTv() {
-  fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=1`)
+  fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((tv, index) => {
         if (tv.poster_path != null) {
@@ -125,7 +276,7 @@ function ratingTopLowTv() {
           image.src = `${imgUrl}${tv.poster_path}`;
           image.classList.add("exampleimg");
 
-          image.addEventListener("click", () => { });
+          image.addEventListener("click", () => {});
 
           movieblock1.appendChild(image);
         }
@@ -133,15 +284,18 @@ function ratingTopLowTv() {
     });
 }
 //get movie by name
+
 function getMovieByName(name) {
   fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=d5dc762873106644192a916a78a39251&language=en-US&query=${name}&api_key=d5dc762873106644192page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=d5dc762873106644192a916a78a39251&language=en-US&query=${name}&api_key=d5dc762873106644192page=${page}&include_adult=false`
   )
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((movie, index) => {
         if (movie.poster_path != null) {
@@ -160,12 +314,16 @@ function getMovieByName(name) {
 }
 //get tv by name
 function getTvByName(name) {
-  fetch(`https://api.themoviedb.org/3/search/tv?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=1&query=${name}&include_adult=false`)
+  fetch(
+    `https://api.themoviedb.org/3/search/tv?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}&query=${name}&include_adult=false`
+  )
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((tv, index) => {
         if (tv.poster_path != null) {
@@ -173,7 +331,7 @@ function getTvByName(name) {
           image.src = `${imgUrl}${tv.poster_path}`;
           image.classList.add("exampleimg");
 
-          image.addEventListener("click", () => { });
+          image.addEventListener("click", () => {});
 
           movieblock1.appendChild(image);
         }
@@ -181,13 +339,25 @@ function getTvByName(name) {
     });
 }
 //get all popular movies
+const title = document.querySelector(".popup-title");
+const imagecard = document.querySelector(".image");
+const rating = document.querySelector(".rating1");
+const summary = document.querySelector(".summary1");
+const genre = document.querySelector(".genre1");
+const director = document.querySelector(".director1");
+const cast = document.querySelector("cast1");
+const releaseyear = document.querySelector("year");
+const video1 = document.querySelector("trailer");
+
 function getPopularMovies() {
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=1`)
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((movie, index) => {
         if (movie.poster_path != null) {
@@ -196,6 +366,9 @@ function getPopularMovies() {
           image.classList.add("exampleimg");
 
           image.addEventListener("click", () => {
+            title.textContent = movie.title;
+            imagecard.src = `${imgUrl}${movie.poster_path}`;
+            summary.textContent = movie.overview;
             openCard();
           });
 
@@ -206,12 +379,14 @@ function getPopularMovies() {
 }
 //get all popular tv shows
 function getPopularTvs() {
-  fetch(`https://api.themoviedb.org/3/tv/popular?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=1`)
+  fetch(`https://api.themoviedb.org/3/tv/popular?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
       const arrmovie = json.results;
       arrmovie.forEach((movie, index) => {
         if (movie.poster_path != null) {
@@ -219,7 +394,56 @@ function getPopularTvs() {
           image.src = `${imgUrl}${movie.poster_path}`;
           image.classList.add("exampleimg");
 
-          image.addEventListener("click", () => { });
+          image.addEventListener("click", () => {});
+
+          movieblock1.appendChild(image);
+        }
+      });
+    });
+}
+//upcoming movies
+function upcomingMovie() {
+  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}`)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
+      const arrmovie = json.results;
+      arrmovie.forEach((movie, index) => {
+        if (movie.poster_path != null) {
+          const image = document.createElement("img");
+          image.src = `${imgUrl}${movie.poster_path}`;
+          image.classList.add("exampleimg");
+
+          image.addEventListener("click", () => {});
+
+          movieblock1.appendChild(image);
+        }
+      });
+    });
+}
+//upcoming tv shows
+function airingTv() {
+  //gets all TV shows that has an episode with an air date in the next 7 days.
+  fetch(`https://api.themoviedb.org/3/tv/on_the_air?api_key=d5dc762873106644192a916a78a39251&language=en-US&page=${page}}`)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    .then((json) => {
+      total = json["total_pages"];
+      console.log(json["total_pages"]);
+      const arrmovie = json.results;
+      arrmovie.forEach((movie, index) => {
+        if (movie.poster_path != null) {
+          const image = document.createElement("img");
+          image.src = `${imgUrl}${movie.poster_path}`;
+          image.classList.add("exampleimg");
+
+          image.addEventListener("click", () => {});
 
           movieblock1.appendChild(image);
         }
@@ -241,19 +465,14 @@ function closeCard() {
 }
 
 // basic paging logic to demo the buttons
-var pr = document.querySelector(".paginate.left");
-var pl = document.querySelector(".paginate.right");
 
 pr.onclick = slide.bind(this, -1);
 pl.onclick = slide.bind(this, 1);
 
-var index = 0,
-  total = 5;
-
 function slide(offset) {
   index = Math.min(Math.max(index + offset, 0), total - 1);
-
-  document.querySelector(".counter").innerHTML = index + 1 + " / " + total;
+  counter1.innerHTML = index + 1 + " / " + total;
+  // page = counter1.textContent.split("/")[0];
 
   pr.setAttribute("data-state", index === 0 ? "disabled" : "");
   pl.setAttribute("data-state", index === total - 1 ? "disabled" : "");
